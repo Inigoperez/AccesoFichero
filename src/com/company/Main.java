13a4d34d570231c23f48039c227575244c28482d
2dam3 <ikbem@plaiaundi.net>
2020-09-29 12:55:00 +0200

    primer commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int seleccion = 0;
+        Scanner sc = new Scanner(System.in);
+
+        while(seleccion == 0) {
+            try{
+                System.out.println("Elige opción:\n1.- Mostrar usuarios" + "\n2.- Añadir usuario\n" + "3.- Eliminar usuario (mediante ID)\n" + "4.- Informacion de usuario (mediante ID)\n" + "5.- Cambiar nombre usuario (Mediante ID)\n"+"5.- Salir");
+                seleccion = sc.nextInt();
+
+                //Ejemplo de switch case en Java
+                switch(seleccion){
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+                        System.out.println("Adios!");
+                        break;
+                    default:
+                        System.out.println("Número no reconocido");break;
+                }
+            }catch(Exception e){
+                System.out.println("Uoop! Error!");
+            }
+        }
+    }
+
+    public void CargarUsuarios(){
+
+    }
+}
