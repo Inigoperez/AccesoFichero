dfd31944d503d3ee67782b3c94f53783498e191d
2dam3 <ikbem@plaiaundi.net>
2020-10-02 09:54:06 +0200

    intento de leer usuarios

--- src/MostrarUsuarios.java
+++ src/MostrarUsuarios.java
@@ -11,24 +11,23 @@ public class MostrarUsuarios {
         Object obj = null;
         try {
             obj = new JSONParser().parse(new FileReader("usuarios.json"));
+            System.out.println(obj);
+            JSONObject jo = (JSONObject) obj;
+
+            for(int i=0;i<jo.size();i++){
+                // getting firstName and lastName
+                String firstName = (String) jo.get("Nombre");
+                String id = (String) jo.get("Id");
+                String localidad = (String) jo.get("Localidad");
+                String edad = (String) jo.get("Edad");
+                System.out.println(firstName);
+                System.out.println(id);
+                System.out.println(localidad);
+                System.out.println(edad);
+            }
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
-
-        // typecasting obj to JSONObject
-        JSONObject jo = (JSONObject) obj;
-
-        // getting firstName and lastName
-        String firstName = (String) jo.get("Nombre");
-        String id = (String) jo.get("Id");
-        String localidad = (String) jo.get("Localidad");
-        String edad = (String) jo.get("Edad");
-
-        System.out.println(firstName);
-        System.out.println(id);
-        System.out.println(localidad);
-        System.out.println(edad);
-
-        }
     }
+}
 
